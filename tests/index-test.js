cd3ba880afada816120bb96e481bcadee51831da
Valentino Valentin <valentinovalentine95@gmail.com>
2017-12-12 10:52:19 -0700

    adding letters

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,5 +1,18 @@
 import { expect } from 'chai';
 import index from '../lib/index'
+import { generateRandomNumbers, generateRandomLetters } from '@valentinovtino/gen-random'
+
 
 describe('Example Test File', () => {
+  it('should generate random numbers', () => {
+    let array = generateRandomNumbers(500, 4000);
+
+    expect(array.length).to.equal(500);
+  })
+  it('should generate random letters', () => {
+    let array = generateRandomLetters(500);
+
+    expect(array.length).to.equal(500);
+  })
 })
+
